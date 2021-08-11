5b45fe07707b828e794fac0e9ee5cadcb7ff3b60
Kishalay <kishalaydas@iisc.ac.in>
2021-08-11 18:04:53 +0530

    Push

--- cif.py
+++ cif.py
@@ -6,23 +6,26 @@ from ase.io import *
 import numpy as np
 import ase.io.vasp
 
-#### Kishalay, please install ase (atomistic simulation environment)
-### I hope it is already installed in
-## your machine as you are using pymatgen
-## other wise make a pip install
-## Run this script in the directory
-## where all the .POSCAR files are there
-
-path='data_poscar/'
+path='../data_poscar/'
 list=[]
 for file in os.listdir('%s'%(path)):
     if fnmatch.fnmatch(file, '*.vasp'):
-       list.append(file)
+        name = int(file.split('.')[0])
+        bulk = read(path + str(file), format='vasp')
+        filename = '../data_cif/' + str(name) + '.cif'
+        ase.io.write(filename, bulk)
 
-print(np.size(list))
 
-for z in list:
-    name=int(z.split('.')[0])
-    bulk=read('data_poscar/'+str(z),format='vasp')
-    filename='data_cif/'+str(name)+'.cif'
-    ase.io.write(filename,bulk)
+# path='data_poscar/'
+# list=[]
+# for file in os.listdir('%s'%(path)):
+#     if fnmatch.fnmatch(file, '*.vasp'):
+#        list.append(file)
+#
+# print(np.size(list))
+#
+# for z in list:
+#     name=int(z.split('.')[0])
+#     bulk=read('data_poscar/'+str(z),format='vasp')
+#     filename='data_cif/'+str(name)+'.cif'
+#     ase.io.write(filename,bulk)
